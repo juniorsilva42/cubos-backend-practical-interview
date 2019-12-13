c63fea1f02f61dcdd8585f521d64219e26d65935
jsiilva1 <jsiilva@outlook.com.br>
2019-12-12 22:02:36 -0300

    Added functions to DI container

--- src/container.js
+++ src/container.js
@@ -1,11 +1,13 @@
 import { createContainer, asFunction } from 'awilix';
 
 import app from './server';
+import logging from './infra/logging';
 
 const container = createContainer();
 
 container.register({
   app: asFunction(app).singleton(),
+  logger: asFunction(logging).singleton(),
 });
 
 module.exports = container;
